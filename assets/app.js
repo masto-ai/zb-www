644a9d89767f8546e36c2be768d187fbc5376bfb
A Kriya <kriya@dismail.de>
2020-12-07 23:09:27 +0530

    Add Contact Us page and form

--- assets/app.js
+++ assets/app.js
@@ -17,36 +17,36 @@
  ===========================*/
 
 ! function($) {
-    "use strict"; 
-    // Loader 
+    "use strict";
+    // Loader
     $(window).on('load', function() {
         $('#status').fadeOut();
         $('#preloader').delay(350).fadeOut('slow');
         $('body').delay(350).css({
             'overflow': 'visible'
         });
-    }); 
-    
+    });
+
     // Menu
     $('.navbar-toggle').on('click', function (event) {
         $(this).toggleClass('open');
         $('#navigation').slideToggle(400);
     });
-    
+
     $('.navigation-menu>li').slice(-1).addClass('last-elements');
-    
+
     $('.menu-arrow,.submenu-arrow').on('click', function (e) {
         if ($(window).width() < 992) {
             e.preventDefault();
             $(this).parent('li').toggleClass('open').find('.submenu:first').toggleClass('open');
         }
     });
-    
+
     $(".navigation-menu a").each(function () {
         if (this.href == window.location.href) {
-            $(this).parent().addClass("active"); 
-            $(this).parent().parent().parent().addClass("active"); 
-            $(this).parent().parent().parent().parent().parent().addClass("active"); 
+            $(this).parent().addClass("active");
+            $(this).parent().parent().parent().addClass("active");
+            $(this).parent().parent().parent().parent().parent().addClass("active");
         }
     });
 
@@ -89,11 +89,11 @@
         } else {
             $('.back-to-top').fadeOut();
         }
-    }); 
+    });
     $('.back-to-top').click(function(){
-        $("html, body").animate({ scrollTop: 0 }, 3000);
+        $("html, body").animate({ scrollTop: 0 }, 1000);
         return false;
-    }); 
+    });
 
     //Tooltip
     $(function () {
